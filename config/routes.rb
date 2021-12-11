c017a6113a3d566d9aeea9b35ed2411d3808a949
kokoira <95961416+kokoira@users.noreply.github.com>
2021-12-11 10:28:09 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
+  #==================削除orコメントアウト================
+  # get 'favorites/create'
+  # get 'favorites/destroy' 
+  #=====================================
+  root 'tweets#index'
+  devise_for :users
+
+  # ================ここをネスト(入れ子)した形に変更
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+    resource :comments, only: [:create, :destroy]
+  end
+  #======================================
+
+  resources :users 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
